7fb3bf5cea64dd16d0ce86d4288831b4d4e4821c
markdekuijer <31616400+markdekuijer@users.noreply.github.com>
2018-09-18 08:40:33 +0200

    BaseReading

---
+++ RythmGame/Assets/Code/MapReader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapReader : MonoBehaviour
+{
+    private void Awake()
+    {
+        //get map data
+    }
+}
+
+public struct MapData
+{
+    
+}
+
+public struct SliceItem
+{
+    public AnimalTypes animalType;
+}
+
+public enum AnimalTypes
+{
+    fish,
+    weed,
+    rice
+};
